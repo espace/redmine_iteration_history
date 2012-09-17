620bde0eb2727b5f5fc7c629d52f686608dd855a
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2012-09-17 19:29:39 +0200

    removing the logger.info lines

--- lib/version_patch.rb
+++ lib/version_patch.rb
@@ -17,10 +17,6 @@ module VersionPatch
   module InstanceMethods
     def create_history
       if self.changed? && self.changes['effective_date']
-        logger.info "*********************************************************************************************************"
-        tmp = self.inspect
-        logger.info tmp
-        logger.info "############################################################################################################"
         self.iteration_histories.create!(:old_due_date=>self.changes['effective_date'][0],
                                          :new_due_date=>self.effective_date,
                                          :reason=>self.change_reason)
