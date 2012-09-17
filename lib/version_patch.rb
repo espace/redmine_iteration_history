96f6065e7cc296b53dd564c577d133b31d912503
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2012-09-17 18:11:13 +0200

    adding some loggs to debug in the server

--- lib/version_patch.rb
+++ lib/version_patch.rb
@@ -10,17 +10,22 @@ module VersionPatch
       attr_accessor :change_reason
     end
   end
-  
+
   module ClassMethods
   end
-  
+
   module InstanceMethods
     def create_history
       if self.changed? && self.changes['effective_date']
+        logger.info "*********************************************************************************************************"
+        tmp = self.inspect
+        logger.info tmp
+        logger.info "############################################################################################################"
         self.iteration_histories.create!(:old_due_date=>self.changes['effective_date'][0],
                                          :new_due_date=>self.effective_date,
                                          :reason=>self.change_reason)
-      end                                      
+      end
     end
   end
 end
+
